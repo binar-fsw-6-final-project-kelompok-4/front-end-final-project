2d7bac7e36b94c11b4573e86f75ef2f8639be9f5
dhewan <dhewanagumm@gmail.com>
2022-07-15 20:14:07 +0700

    register integration

--- components/produk/AddProduk/index.js
+++ components/produk/AddProduk/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import styles from "./tambahproduk.module.css";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 export default function Tambahproduk(){
+    const {register, handleSubmit} = useForm()
+    const onSubmit = (data) =>{
+      axios.post("https://fsw6-group4-staging.herokuapp.com/api/v1/products",{
+        product_name: data.product_name,
+        price: data.price,
+        category: data.category,
+        product_img1: data.product_img1,
+        description : data.description
+      })
+      console.log(data);
+    }
     return(
         <div className={styles.container}>
             <Head>
@@ -13,20 +26,20 @@ export default function Tambahproduk(){
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
             </Head>
                 <div className={styles.konten}>
-                    <form action="/previewproduk" method="POST">
+                    <form action="/previewproduk" method="POST" onSubmit={handleSubmit(onSubmit)}>
                         <div className={styles.namaproduct}>
                             <label for='namaproduk' className={styles.labelproduct}>Nama Produk</label>
-                            <input type='text' className={styles.inputkonten} name="Nama" id="namaproduk" placeholder="Nama Produk" required></input>
+                            <input type='text' className={styles.inputkonten} name="Nama" id="namaproduk" placeholder="Nama Produk" {...register("product_name")}required></input>
                         </div>
 
                         <div className={styles.hargaproduct}>
                             <label for='hargaproduk' className={styles.labelproduct}>Harga Produk</label>
-                            <input type='text' className={styles.inputkonten} name="Harga" id="hargaproduk" placeholder="Rp. 0.00" required></input>
+                            <input type='text' className={styles.inputkonten} name="Harga" id="hargaproduk" placeholder="Rp. 0.00" {...register("price")}required></input>
                         </div>
 
                         <div className={styles.kategoriproduct}>
                                 <label for="kategori">Kategori</label>
-                                    <select name="kategori" id="kategori" className={styles.inputkonten}    >
+                                    <select name="kategori" id="kategori" className={styles.inputkonten}   {...register("category")} >
                                         <option value=""></option>
                                         <option value="Hobi">Hobi</option>
                                         <option value="Kendaraan">Kendaraan</option>
@@ -37,12 +50,12 @@ export default function Tambahproduk(){
 
                         <div className={styles.deskripsiproduct}>
                             <label for='Deskripsiproduk' className={styles.labelproduct}>Deskripsi Produk</label>
-                            <input type='text' className={styles.inputkonten} name="Deskripsi" id="Deskripsiproduk" placeholder="Contoh : ..." required></input>
+                            <input type='text' className={styles.inputkonten} name="Deskripsi" id="Deskripsiproduk" placeholder="Contoh : ..." {...register("description")}required></input>
                         </div>
 
                         <div className={styles.fotoproduct}>
                                 <label for='fotoproduk' className={styles.labelproductgambar}>Foto Produk</label>
-                                <input type='file' className={styles.inputkontenfile} name="foto" id="fotoproduk" required></input>
+                                <input type='file' className={styles.inputkontenfile} name="foto" id="fotoproduk" {...register("product_img1")}required></input>
                         </div>
 
                         <div className={styles.buttonproduct}>
--- components/register/index.js
+++ components/register/index.js
@@ -1,60 +1,31 @@
 import Head from "next/head"
 import Styles from "./register.module.css"
 import Link from "next/link"
-import { useState } from 'react';
-import { useRouter } from 'next/router'
+// import { useState } from 'react';
+// import { useRouter } from 'next/router'
+import { useForm } from "react-hook-form";
 import axios from "axios";
+import toast, {Toaster} from "react-hot-toast"
 
 export default function register(){
-    // function doLogin({ email, password, username }) {
-    //     // Gunakan endpoint-mu sendiri
-    //     const registData = {
-    //       email : email,
-    //       password : password,
-    //       username : username
-    //     }   
-    //     axios.post("https://fsw6-group4-staging.herokuapp.com/api/v1/users/add",registData)
-    //     };
-      
-      
-const [usernameData, setUsername] = useState('');
-  const [emailData, setEmail] = useState('');
-  const [passwordData, setPassword] = useState('');
- const router = useRouter()
-  // States for checking the errors
+  const {register, handleSubmit} = useForm()
+  const onSubmit = (data) =>{
+    axios.post("https://fsw6-group4-staging.herokuapp.com/api/v1/users/add",{
+        email :data.email,
+        password : data.password,
+        username : data.username
+    })
+    toast.success("Regsiter Berhasil", {
+        duration: 4000,
+        position: 'top-center',
+    })
+    window.setTimeout(function(){
+        window.location.href = "/login";
+    }, 2000);
+    console.log(data);
+  }
 
- 
-  // Handling the name change
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-  };
- 
-  // Handling the email change
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
- 
-  // Handling the password change
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
- 
-  // Handling the form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (usernameData === '' || emailData === '' || passwordData === '') {
-      setError(true);
-    } else {
-         const registData = {
-          email : emailData,
-          password : passwordData,
-          username : usernameData
-        }   
-      axios.post("https://fsw6-group4-staging.herokuapp.com/api/v1/users/add",registData)
-      router.push('/login')
-    }
-  };
- 
+
  
     return(
         <div className={Styles.container}>
@@ -62,13 +33,14 @@ const [usernameData, setUsername] = useState('');
                 <title>SecondHand. | Register</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>   
+            <Toaster />
                 <div className={Styles.card}>
                     <div className={Styles.judul}>
                         <h1>SecondHand.</h1>
                     </div>
 
                     <div className={Styles.form}>
-                        <form action="login.js">
+                        <form action="login.js" onSubmit={handleSubmit(onSubmit)}>
                             <div className={Styles.box}>
                                 <div className={Styles.header}>
                                     <h2 align="center">REGISTER</h2>
@@ -76,14 +48,14 @@ const [usernameData, setUsername] = useState('');
 
                                 <div className={Styles.username} align="center">
                                     <label  className={Styles.form_label}>Username</label><br></br>
-                                    <input type="text" value={usernameData} onChange={handleUsername} className={Styles.form_control} name="username" id="username" placeholder="Masukkan Username Anda" required></input>
+                                    <input type="text"  className={Styles.form_control} name="username" id="username" placeholder="Masukkan Username Anda" required {...register("username")}></input>
                                 </div>
 
                                 <br></br>
 
                                 <div className={Styles.email} align="center">
                                     <label  className={Styles.form_label}>E-mail</label><br></br>
-                                    <input type="text" value={emailData} onChange={handleEmail}className={Styles.form_control} name="Email" id="Email" placeholder="Masukkan Email Anda" required></input>
+                                    <input type="text" className={Styles.form_control} name="Email" id="Email" placeholder="Masukkan Email Anda" required {...register("email")}></input>
                                 </div>
 
                                 <br>
@@ -91,11 +63,11 @@ const [usernameData, setUsername] = useState('');
 
                                 <div className={Styles.password} align="center">
                                     <label  className={Styles.form_label}>Password</label><br></br>
-                                    <input type="password" value={passwordData} onChange={handlePassword} className={Styles.form_control} name="password" id="password" placeholder="Masukkan Password Anda" required></input>
+                                    <input type="password"  className={Styles.form_control} name="password" id="password" placeholder="Masukkan Password Anda" required {...register("password")}></input>
                                 </div>
                                 <br></br>
                                 <div className={Styles.button} align="center">
-                                    <input type="submit" onClick={handleSubmit} className={Styles.button1} name="submit" value="Masuk"></input>
+                                    <input type="submit"  className={Styles.button1} name="submit" value="Masuk"></input>
                                 </div>
                                     
                                 <div className={Styles.page} align="center">
