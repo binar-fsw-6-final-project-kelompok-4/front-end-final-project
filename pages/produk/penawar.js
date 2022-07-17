f2028653fcfb21a318c0d9ab18fc6931841cd558
DyB07 <dhaniel.y.bawias@gmail.com>
2022-07-17 15:15:44 +0800

    membuat responsive penawar & membuat notifikasi

--- pages/produk/penawar.js
+++ pages/produk/penawar.js
@@ -1,13 +1,7 @@
-import Navbar from "../../components/Navbar/Navbar";
 import Penawar from "../../components/Penawar"
-import Footer from "../../components/Footer/Footer";
 
 export default function penawar() {
     return (
-        <div>
-            <Navbar />
-            <Penawar />
-            <Footer />
-        </div>
+        <Penawar />
     )
 }
