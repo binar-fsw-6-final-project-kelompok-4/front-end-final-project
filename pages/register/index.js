61397bb216a12b9b90b3d160a57d5186c0912e4e
RyanRamadhan11 <ramadhanryan676@gmail.com>
2022-07-22 12:47:52 +0700

    update integrasi, deploy

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,9 +1,5 @@
 import Register from "../../components/register"
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b84d5bb62e3c7323848af43d4b1667d2de820b13
 export default function index() {
     return (
         <Register />
